615d8aa29185820f47447ae1fe6eccc60466b738
MatejPemper <90246393+MatejPemper@users.noreply.github.com>
2022-10-10 22:41:37 +0200

    Add files via upload

---
+++ ZadatakZaVjezbu2/ZadatakZaVjezbu2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaVjezbu2
+{
+    internal class Program
+    {
+
+        class KlasaB
+        {
+            public string BezPrvogIZadnjeg(string prviIZadnji)
+            {
+                prviIZadnji = prviIZadnji.Remove(prviIZadnji.Length - 1, 1);
+
+                prviIZadnji = prviIZadnji.Remove(0, 1);
+
+                return prviIZadnji;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            KlasaB klasaB = new KlasaB();
+
+            Console.WriteLine("Unesi niz znakova: ");
+            string a = Console.ReadLine();
+
+            if(a.Length > 2)
+            {
+                Console.WriteLine(klasaB.BezPrvogIZadnjeg(a));
+
+            }
+            else
+            {
+                Console.WriteLine("Unesite više od dva znaka");
+            }
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
